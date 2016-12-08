28f6adeed1e2ff7f8ebcac162b04f13336085888
边雷 <775884216@qq.com>
2016-12-08 21:17:33 +0800

    界面搭建

--- PuBuLiu/PuBuLiu/ViewController.m
+++ PuBuLiu/PuBuLiu/ViewController.m
@@ -19,8 +19,27 @@
    
 }
 
+#pragma mark - 数据源方法
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 100;
+}
 
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor redColor];
+    
+    return cell;
+}
 
-
+#pragma mark - footerView
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    // 参数一在UICollectionViewLayout中已经定义
+    UICollectionReusableView *footerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"footer" forIndexPath:indexPath];
+    return footerView;
+}
 
 @end
